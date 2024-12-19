ae93e84c44fa4d2f4f40abbb6d2a74767676d4fa
hereisth <tanghao95@outlook.com>
2024-12-19 18:23:52 +0800

    feat: Enhance AppHeader with new buttons for board management and theme switching; update ThemeSwitcher styles for better accessibility

--- app/(app)/dashboard/_components/app-header.tsx
+++ app/(app)/dashboard/_components/app-header.tsx
@@ -1,9 +1,47 @@
+import { ThemeSwitcher } from "@/components/theme-switcher";
+import { Button } from "@/components/ui/button";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Edit, Grid2x2Icon, List } from "lucide-react";
 
 export function AppHeader() {
   return (
     <div className="flex justify-between items-center w-full">
-      <SidebarTrigger />
+      <div className="flex items-center gap-2">
+        <SidebarTrigger />
+      </div>
+
+      <div className="flex items-center">
+        <Button
+          variant="ghost"
+          onClick={() => {
+            // create a new board item in supabase
+            console.log("create a new board item in supabase");
+          }}
+        >
+          <Edit className="w-4 h-4" />
+        </Button>
+
+        <Button
+          variant="ghost"
+          onClick={() => {
+            // change the baords view to grid
+            console.log("change the baords view to grid");
+          }}
+        >
+          <Grid2x2Icon className="w-4 h-4" />
+        </Button>
+
+        <Button
+          variant="ghost"
+          onClick={() => {
+            // change the baords view to list
+            console.log("change the baords view to list");
+          }}
+        >
+          <List className="w-4 h-4" />
+        </Button>
+        <ThemeSwitcher />
+      </div>
     </div>
   );
 }
--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -35,19 +35,19 @@ const ThemeSwitcher = () => {
             <Sun
               key="light"
               size={ICON_SIZE}
-              className={"text-muted-foreground"}
+              className="outline-none"
             />
           ) : theme === "dark" ? (
             <Moon
               key="dark"
               size={ICON_SIZE}
-              className={"text-muted-foreground"}
+              className="outline-none"
             />
           ) : (
             <Laptop
               key="system"
               size={ICON_SIZE}
-              className={"text-muted-foreground"}
+              className="outline-none"
             />
           )}
         </Button>
